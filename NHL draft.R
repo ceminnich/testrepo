2c8c7d870c7038786c1d1873b18d6664816ca74a
ceminnich <ceminnich@gmail.com>
2018-05-15 09:18:14 -0700

    test commit

--- NHL draft.R
+++ NHL draft.R
@@ -1,3 +1,4 @@
+
 pickOdds <- data.frame(team=c(1:15),pick1=c(.185,.135,.115,.095,.085,.075,.065,.06,.05,.035,.03,.025,.02,.015,.01))
 draftOrder <- data.frame(pick1=0,pick2=0,pick3=0,prob1=0,prob2=0,prob3=0)
 draftOrder <- draftOrder[0,]
